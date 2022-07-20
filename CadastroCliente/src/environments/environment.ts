694dae289175e967926d2c529377f782474fb047
robertonunes449 <roberto.nunes449@gmail.com>
2022-07-20 09:51:25 -0300

    Atualização dos dados do component colaborador

--- CadastroCliente/src/environments/environment.ts
+++ CadastroCliente/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   baseUrl: 'https://cadastrocliente-api.herokuapp.com/'
-};
+}; 
 
 /*
  * For easier debugging in development mode, you can import the following file
